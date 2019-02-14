9db7e9e8a4e7eb43bd8d819b9e6e24f96a13c627
Mihael Isaev <isaev.mihael@gmail.com>
2019-02-15 01:36:26 +0400

    Implement PostgresArray object

---
+++ Sources/SwifQL/PostgresArray.swift
@@ -0,0 +1,36 @@
+//
+//  PostgresArray.swift
+//  SwifQL
+//
+//  Created by Mihael Isaev on 14/02/2019.
+//
+
+import Foundation
+
+public typealias PgArray = PostgresArray
+
+public struct PostgresArray: SwifQLable {
+    public var parts: [SwifQLPart] = []
+    public init (_ items: SwifQLable...) {
+        self.init(items)
+    }
+    public init (_ items: [SwifQLable]) {
+        guard items.count > 0 else {
+            parts.append(o: .doubleDollar)
+            parts.append(o: .openSquareBracket)
+            parts.append(o: .closeSquareBracket)
+            parts.append(o: .doubleDollar)
+            return
+        }
+        parts.append(o: .array)
+        parts.append(o: .openSquareBracket)
+        for (i, v) in items.enumerated() {
+            if i > 0 {
+                parts.append(o: .comma)
+                parts.append(o: .space)
+            }
+            parts.append(contentsOf: v.parts)
+        }
+        parts.append(o: .closeSquareBracket)
+    }
+}
