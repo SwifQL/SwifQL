fae07775433d5442f0566e97a6822dce7ad1ec9c
Mihael Isaev <isaev.mihael@gmail.com>
2019-02-15 01:35:14 +0400

    SwifQLable: implement `groupBy` method

---
+++ Sources/SwifQL/SwifQLable+GroupBy.swift
@@ -0,0 +1,30 @@
+//
+//  SwifQLable+GroupBy.swift
+//  SwifQL
+//
+//  Created by Mihael Isaev on 14/02/2019.
+//
+
+import Foundation
+
+//MARK: GROUP BY
+
+extension SwifQLable {
+    public func groupBy(_ fields: SwifQLable...) -> SwifQLable {
+        return groupBy(fields)
+    }
+    public func groupBy(_ fields: [SwifQLable]) -> SwifQLable {
+        var parts = self.parts
+        parts.appendSpaceIfNeeded()
+        parts.append(o: .groupBy)
+        parts.append(o: .space)
+        for (i, v) in fields.enumerated() {
+            if i > 0 {
+                parts.append(o: .comma)
+                parts.append(o: .space)
+            }
+            parts.append(contentsOf: v.parts)
+        }
+        return SwifQLableParts(parts: parts)
+    }
+}
