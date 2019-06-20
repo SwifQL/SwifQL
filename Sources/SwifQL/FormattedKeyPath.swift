0078cabf58ebc2db9474c46973e492a24ec6f434
Mihael Isaev <isaev.mihael@gmail.com>
2019-06-20 19:02:24 +0400

    Implement `FormattedKeyPath`

---
+++ Sources/SwifQL/FormattedKeyPath.swift
@@ -0,0 +1,40 @@
+//
+//  FormattedKeyPath.swift
+//  SwifQL
+//
+//  Created by Mihael Isaev on 20/06/2019.
+//
+
+import Foundation
+
+/// Formatting keypath
+public typealias FKP = FormattedKeyPath
+
+public struct FormattedKeyPath {
+    let _table: String
+    let _paths: [String]
+    
+    public init <T: Tableable>(_ table: T.Type, _ paths: String...) {
+        _table = table.entity
+        _paths = paths
+    }
+    
+    public init <T: Tableable>(_ table: T.Type, _ paths: [String]) {
+        _table = table.entity
+        _paths = paths
+    }
+    
+    public init (_ table: String, _ paths: String...) {
+        _table = table
+        _paths = paths
+    }
+    
+    public init (_ table: String, _ paths: [String]) {
+        _table = table
+        _paths = paths
+    }
+}
+
+extension FormattedKeyPath: SwifQLable {
+    public var parts: [SwifQLPart] { return [SwifQLPartKeyPath(table: _table, paths: _paths)] }
+}
