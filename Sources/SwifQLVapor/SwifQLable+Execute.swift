00a6c73818f4fd222cde03030c6012ca2a88cc00
Mihael Isaev <isaev.mihael@gmail.com>
2019-07-18 00:36:56 +0400

    Implement `run` with `Future<Void>` for Vapor

--- Sources/SwifQLVapor/SwifQLable+Execute.swift
+++ Sources/SwifQLVapor/SwifQLable+Execute.swift
@@ -20,11 +20,6 @@ extension SwifQLable {
         return execute(on: conn, as: .mysql)
     }
     
-    fileprivate func execute<C>(on conn: C, as dialect: SQLDialect) -> SQLRawBuilder<C> where C: SQLConnectable {
-        let prepared = prepare(dialect).splitted
-        return conn.raw(prepared.query).binds(prepared.values)
-    }
-    
     public func execute<D: PostgreSQLDatabase>(on container: Container, as identifier: DatabaseKit.DatabaseIdentifier<D>) -> Future<SQLRawBuilder<D.Connection>> {
         return container.requestPooledConnection(to: identifier).flatMap { conn in
             defer { try? container.releasePooledConnection(conn, to: identifier) }
@@ -37,5 +32,32 @@ extension SwifQLable {
             defer { try? container.releasePooledConnection(conn, to: identifier) }
             return container.eventLoop.newSucceededFuture(result: self.execute(on: conn))
         }
+    }
+    
+    public func run<C>(on conn: C) -> Future<Void> where C: PostgreSQLConnection {
+        return execute(on: conn, as: .psql).run()
+    }
+    
+    public func run<C>(on conn: C) -> Future<Void> where C: MySQLConnection {
+        return execute(on: conn, as: .mysql).run()
+    }
+    
+    public func run<D: PostgreSQLDatabase>(on container: Container, as identifier: DatabaseKit.DatabaseIdentifier<D>) -> Future<Void> {
+        return container.requestPooledConnection(to: identifier).flatMap { conn in
+            defer { try? container.releasePooledConnection(conn, to: identifier) }
+            return self.run(on: conn)
+        }
+    }
+    
+    public func run<D: MySQLDatabase>(on container: Container, as identifier: DatabaseKit.DatabaseIdentifier<D>) -> Future<Void> {
+        return container.requestPooledConnection(to: identifier).flatMap { conn in
+            defer { try? container.releasePooledConnection(conn, to: identifier) }
+            return self.run(on: conn)
+        }
+    }
+    
+    fileprivate func execute<C>(on conn: C, as dialect: SQLDialect) -> SQLRawBuilder<C> where C: SQLConnectable {
+        let prepared = prepare(dialect).splitted
+        return conn.raw(prepared.query).binds(prepared.values)
     }
 }
