f4221551fc82c72faaae83143c3bc5375b11c5f1
Mihael Isaev <isaev.mihael@gmail.com>
2019-08-02 19:47:57 +0400

    SwifQLable: add `timestamp`

---
+++ Sources/SwifQL/SwifQLable+Timestamp.swift
@@ -0,0 +1,36 @@
+//
+//  SwifQLable+Timestamp.swift
+//  SwifQL
+//
+//  Created by Mihael Isaev on 02/08/2019.
+//
+
+import Foundation
+
+//MARK: Timestamp
+
+extension SwifQLable {
+    public var timestamp: SwifQLable {
+        var parts = self.parts
+        parts.appendSpaceIfNeeded()
+        parts.append(o: .timestamp)
+        return SwifQLableParts(parts: parts)
+    }
+    public func timestamp(_ fields: SwifQLable...) -> SwifQLable {
+        return timestamp(fields)
+    }
+    public func timestamp(_ fields: [SwifQLable]) -> SwifQLable {
+        var parts = self.parts
+        parts.appendSpaceIfNeeded()
+        parts.append(o: .timestamp)
+        parts.append(o: .space)
+        for (i, v) in fields.enumerated() {
+            if i > 0 {
+                parts.append(o: .comma)
+                parts.append(o: .space)
+            }
+            parts.append(contentsOf: v.parts)
+        }
+        return SwifQLableParts(parts: parts)
+    }
+}
