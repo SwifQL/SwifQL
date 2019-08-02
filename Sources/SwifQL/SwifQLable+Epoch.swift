3422ccd8433536f33210a53d41757f0ca0ffacff
Mihael Isaev <isaev.mihael@gmail.com>
2019-08-02 19:47:19 +0400

    SwifQLable: add `epoch`

---
+++ Sources/SwifQL/SwifQLable+Epoch.swift
@@ -0,0 +1,29 @@
+//
+//  SwifQLable+Epoch.swift
+//  SwifQL
+//
+//  Created by Mihael Isaev on 02/08/2019.
+//
+
+import Foundation
+
+//MARK: Epoch
+
+extension SwifQLable {
+    public var epoch: SwifQLable {
+        var parts = self.parts
+        parts.appendSpaceIfNeeded()
+        parts.append(o: .epoch)
+        return SwifQLableParts(parts: parts)
+    }
+    public func epoch(with value: SwifQLable) -> SwifQLable {
+        var parts = self.parts
+        parts.appendSpaceIfNeeded()
+        parts.append(o: .custom(Op.epoch.value.singleQuotted))
+        parts.append(o: .space)
+        parts.append(o: .custom("+"))
+        parts.append(o: .space)
+        parts.append(contentsOf: value.parts)
+        return SwifQLableParts(parts: parts)
+    }
+}
