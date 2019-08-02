228b7865e87d0e8dfbd8768d9104827cdb921e3a
Mihael Isaev <isaev.mihael@gmail.com>
2019-08-02 19:47:47 +0400

    SwifQLable: add `overlaps`

---
+++ Sources/SwifQL/SwifQLable+Overlaps.swift
@@ -0,0 +1,43 @@
+//
+//  SwifQLable+Overlaps.swift
+//  SwifQL
+//
+//  Created by Mihael Isaev on 02/08/2019.
+//
+
+import Foundation
+
+//MARK: Overlaps
+
+extension SwifQLable {
+    public var overlaps: SwifQLable {
+        var parts = self.parts
+        parts.appendSpaceIfNeeded()
+        parts.append(o: .overlaps)
+        return SwifQLableParts(parts: parts)
+    }
+    public func overlaps(_ fields: SwifQLable...) -> SwifQLable {
+        return overlaps(fields)
+    }
+    public func overlaps(_ fields: [SwifQLable]) -> SwifQLable {
+        var parts = self.parts
+        parts.appendSpaceIfNeeded()
+        parts.append(o: .overlaps)
+        if fields.count > 0 {
+            parts.append(o: .space)
+            parts.append(o: .openBracket)
+        }
+        for (i, v) in fields.enumerated() {
+            if i > 0 {
+                parts.append(o: .comma)
+                parts.append(o: .space)
+            }
+            parts.append(contentsOf: v.parts)
+        }
+        if fields.count > 0 {
+            parts.append(o: .closeBracket)
+        }
+        return SwifQLableParts(parts: parts)
+    }
+}
+
