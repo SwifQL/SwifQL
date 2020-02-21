7ed40af7ba1d425b657968fe4196dd258748be58
Mihael Isaev <isaev.mihael@gmail.com>
2020-02-22 02:05:45 +0400

    Improve join syntax

--- Sources/SwifQL/SwifQLable+Parts/SwifQLable+Join.swift
+++ Sources/SwifQL/SwifQLable+Parts/SwifQLable+Join.swift
@@ -10,10 +10,10 @@ import Foundation
 //MARK: JOIN
 
 extension SwifQLable {
-    public func join(_ mode: JoinMode, _ table: SwifQLable, on predicates: SwifQLable) -> SwifQLable {
+    public func join(_ mode: JoinMode = .none, _ expression: SwifQLable, on predicates: SwifQLable) -> SwifQLable {
         var parts = self.parts
         parts.appendSpaceIfNeeded()
-        let join = SwifQLJoinBuilder(mode, table, on: predicates)
+        let join = SwifQLJoinBuilder(mode, expression, on: predicates)
         parts.append(contentsOf: join.parts)
         return SwifQLableParts(parts: parts)
     }
