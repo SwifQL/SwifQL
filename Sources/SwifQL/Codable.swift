c1cf37aeccc0025ce88ba348a6f4cf1ce3f42fdf
Mihael Isaev <isaev.mihael@gmail.com>
2020-04-25 18:30:44 +0400

    Implement `SwifQLCodable`

---
+++ Sources/SwifQL/Codable.swift
@@ -0,0 +1,20 @@
+//
+//  Codable.swift
+//  SwifQL
+//
+//  Created by Mihael Isaev on 25.04.2020.
+//
+
+import Foundation
+
+public protocol SwifQLCodable: Codable, SwifQLable {}
+
+extension SwifQLCodable {
+    public var parts: [SwifQLPart] { [SwifQLPartUnsafeValue(self)] }
+}
+
+public protocol SwifQLEncodable: Encodable, SwifQLable {}
+
+extension SwifQLEncodable {
+    public var parts: [SwifQLPart] { [SwifQLPartUnsafeValue(self)] }
+}
