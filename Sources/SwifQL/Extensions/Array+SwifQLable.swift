0b8ab4d8100c28a18284030a074a8195ebebef92
Tierra Cero <vcantu01@tierracero.com>
2023-09-03 16:45:36 -0600

    Update Array+SwifQLable.swift
    Removed request  spaces

--- Sources/SwifQL/Extensions/Array+SwifQLable.swift
+++ Sources/SwifQL/Extensions/Array+SwifQLable.swift
@@ -8,7 +8,6 @@
 import Foundation
 
 extension Array: SwifQLable where Element: SwifQLable {
-    
     public var parts: [SwifQLPart] {
         if let _ = Element.self as? AnySwifQLEnum.Type {
             let values = compactMap {
@@ -16,11 +15,9 @@ extension Array: SwifQLable where Element: SwifQLable {
             }.joined(separator: ",")
             return [SwifQLPartSafeValue("{\(values)}")]
         }
-        
         if let s = self as? SwifQLCodable {
             return [SwifQLPartUnsafeValue(s)]
         } else {
-            
             return separator(.comma).parts
         }
     }
